74a4e40f9be728bfb42c64d010d6c3938c61d089
MariaLerato <fenyane1999@gmail.com>
2022-02-08 11:14:17 +0200

    make it better

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ import mongodb from 'mongodb'
 import hotels from './routes/hotelRoutes.js'
 import hotelguests from "./routes/hotelGuests.route.js"
 import hotelroom from './routes/hotelRooms.route.js'
-
+import dotenv from 'dotenv'
+import http from 'http'
 const app = express()
 
+dotenv.config()
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.json({limit:"30mb",extended:true}))
@@ -16,6 +20,9 @@ app.use("/api/hotelGuests",hotelguests)
 app.use("/api/v1/hotelRoom",hotelroom)
 
 app.use("*",(req,res)=>res.status(404).json({error:"not found"}))
-
+server.listen(port,()=>{
+    console.log(`Connected to port ${port}`)
+})
 export default app
+
 
